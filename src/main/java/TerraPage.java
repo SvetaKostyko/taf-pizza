c2cfe7738cc6e1693258991a81ae7364e20bc4cf
Sveta <skostyko@gmail.com>
2023-02-19 18:16:45 +0100

    refactor TerraPage , add SIZE_OF_PIZZA

--- src/main/java/TerraPage.java
+++ src/main/java/TerraPage.java
@@ -3,10 +3,9 @@ public class TerraPage {
     public static final String BTN_PIZZA = "//a[@href='/catalog/picca']";
     public static final String BTN_COOKIES = "//button[@class='close-icon']";
     public static final String BTN_MARGARITA_PIZZA = "//button[@data-id='364']";
-    public static final String CHOOSE_CITY_MINSK_BTN = "//button[@data-hrf='/site/change-city?city=city_1']";
     public static final String CART_BTN = "//div[@class='basket__btn-top basket__top basket__btn-top--sm']";
+    public static final String SIZE_OF_PIZZA = "//span[@class='size-product']";
     public static final String TITLE_OF_PIZZA = "//div[@class='basket__products-item-name']";
-
 
 
 }
