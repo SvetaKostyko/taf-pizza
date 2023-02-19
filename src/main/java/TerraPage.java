06b2da79b185174f1c70221e125bed5577bfcb8f
Sveta <skostyko@gmail.com>
2023-02-19 15:36:26 +0100

    refactor TerraPage , add BTN_MARGARITA_PIZZA

--- src/main/java/TerraPage.java
+++ src/main/java/TerraPage.java
@@ -1,5 +1,6 @@
 public class TerraPage {
     public static final String URL = "https://terrapizza.by/";
     public static final String BTN_PIZZA = "//a[@href='/catalog/picca']";
+    public static final String BTN_MARGARITA_PIZZA = "//button[@data-id='364']";
 
 }
