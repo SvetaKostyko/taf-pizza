723e1150f3ac5edfe6f028123369e258332d5c54
Sveta <skostyko@gmail.com>
2023-02-19 15:41:14 +0100

    refactor TerraPage , add BTN_COOKIES

--- src/main/java/TerraPage.java
+++ src/main/java/TerraPage.java
@@ -1,6 +1,8 @@
 public class TerraPage {
     public static final String URL = "https://terrapizza.by/";
     public static final String BTN_PIZZA = "//a[@href='/catalog/picca']";
+    public static final String BTN_COOKIES = "//button[@class='close-icon']";
     public static final String BTN_MARGARITA_PIZZA = "//button[@data-id='364']";
+
 
 }
