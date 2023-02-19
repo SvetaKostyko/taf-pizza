a6015fd30f324ad0e5f52c8d1314ea2b525f6154
Sveta <skostyko@gmail.com>
2023-02-19 12:36:35 +0100

    refactor TerraTest, add setUpBrowser

--- src/test/java/TerraTest.java
+++ src/test/java/TerraTest.java
@@ -1,2 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class TerraTest {
+    WebDriver driver;
+
+    @Before
+    public void setUpBrowser() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(TerraPage.URL);
+    }
+    @Test
+    public void choosePizza(){
+        List<WebElement> pizzaBtn = driver.findElements(By.xpath(TerraPage.BTN_PIZZA));
+        pizzaBtn.get(1).click();
+    }
+    @After
+    public void closeBrowser(){
+    driver.quit();
+    }
 }
