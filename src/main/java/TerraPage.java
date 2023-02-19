cb8a78328a0dbb4b208b037da8167955da50ef1f
Sveta <skostyko@gmail.com>
2023-02-19 15:45:07 +0100

    refactor TerraPage , add CHOOSE_CITY_MINSK_BTN

--- src/main/java/TerraPage.java
+++ src/main/java/TerraPage.java
@@ -3,6 +3,8 @@ public class TerraPage {
     public static final String BTN_PIZZA = "//a[@href='/catalog/picca']";
     public static final String BTN_COOKIES = "//button[@class='close-icon']";
     public static final String BTN_MARGARITA_PIZZA = "//button[@data-id='364']";
+    public static final String CHOOSE_CITY_MINSK_BTN = "//button[@data-hrf='/site/change-city?city=city_1']";
+
 
 
 }
