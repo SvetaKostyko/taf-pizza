89a4118a6672c44b980e49d9adf64b92661b4de0
Sveta <skostyko@gmail.com>
2023-02-19 15:50:13 +0100

    refactor TerraPage , add CART_BTN

--- src/main/java/TerraPage.java
+++ src/main/java/TerraPage.java
@@ -4,6 +4,7 @@ public class TerraPage {
     public static final String BTN_COOKIES = "//button[@class='close-icon']";
     public static final String BTN_MARGARITA_PIZZA = "//button[@data-id='364']";
     public static final String CHOOSE_CITY_MINSK_BTN = "//button[@data-hrf='/site/change-city?city=city_1']";
+    public static final String CART_BTN = "//div[@class='basket__btn-top basket__top basket__btn-top--sm']";
 
 
 
