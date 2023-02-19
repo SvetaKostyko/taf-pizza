17742205b1aa5e2fc9f78a97643c77263dca5a5b
Sveta <skostyko@gmail.com>
2023-02-19 18:20:04 +0100

    refactor TerraTest , add choosePizza

--- src/test/java/TerraTest.java
+++ src/test/java/TerraTest.java
@@ -1,10 +1,15 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 import java.util.List;
 
 public class TerraTest {
@@ -19,7 +24,7 @@ public class TerraTest {
 
     @Test
     public void choosePizza() {
-        WebElement cookiesButton = driver.findElement(By.xpath("//button[@class='close-icon']"));
+        WebElement cookiesButton = driver.findElement(By.xpath(TerraPage.BTN_COOKIES));
         cookiesButton.click();
         List<WebElement> pizzaBtn = driver.findElements(By.xpath(TerraPage.BTN_PIZZA));
         for (WebElement element : pizzaBtn) {
@@ -28,10 +33,19 @@ public class TerraTest {
                 break;
             }
         }
+        WebElement margaritaElement = driver.findElement(By.xpath(TerraPage.BTN_MARGARITA_PIZZA));
+        margaritaElement.click();
+        WebElement cartElement = driver.findElement(By.xpath(TerraPage.CART_BTN));
+        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOf(cartElement));
+        cartElement.click();
+        WebElement size = driver.findElement(By.xpath(TerraPage.SIZE_OF_PIZZA));
+        new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOf(size));
+        WebElement nameOfPizza = driver.findElement(By.xpath(TerraPage.TITLE_OF_PIZZA));
+        Assert.assertEquals("Wrong text", "Пицца Маргарита Классическая 32 см", nameOfPizza.getText());
     }
 
-        @After
-        public void closeBrowser () {
-            driver.quit();
-        }
+    @After
+    public void closeBrowser() {
+        driver.quit();
+    }
 }
